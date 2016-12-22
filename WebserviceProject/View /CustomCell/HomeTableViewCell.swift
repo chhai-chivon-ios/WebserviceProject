79abb96d9f04f0b80bac28e8b5caf2b30c20b15f
Pisith Ly Khmer <kshrd@kshrds-imac-2.local>
2016-12-22 13:46:37 +0700

    Static Table View Commit

---
+++ WebserviceProject/View /CustomCell/HomeTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewCell.swift
+//  WebserviceProject
+//
+//  Created by KSHRD on 12/22/16.
+//  Copyright © 2016 KSHRD. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var homeImageView: UIImageView!
+    @IBOutlet weak var homeTitleLabel: UILabel!
+    @IBOutlet weak var homeDescriptionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
